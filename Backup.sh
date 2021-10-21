adb8ba0dc89b7332cddf5e8d48c9a533f9ac7907
Mehmet Sutaş <46458775+mehmetsutas@users.noreply.github.com>
2021-10-21 16:29:44 +0300

    sutasmarket düzeltmesi

--- Backup.sh
+++ Backup.sh
@@ -2,7 +2,7 @@
 # fecha has a formated date
 fecha=`date +"%d-%m-%Y"`
 # Backup and gzip the directory
-tar zcvf /var/yedek/backups/muhasebe_bup-$fecha.tgz -C / var/www/muhasebe > /dev/null
+tar zcvf /var/yedek/backups/sutasmarket_bup-$fecha.tgz --exclude '/var/www/sutasmarket/image/cache/*' --exclude '/var/www/sutasmarket/image/cache' -C / var/www > /dev/null
 # Rotate the logs, delete older than 7 days
 find /var/yedek/backups/ -mtime +14 -exec rm {} \; 
-rsync -a --delete --password-file=/var/rsync_pass_qnap /var/yedek/ rsync://qnap_rsync@192.168.2.211:873/Muhasebe_NAS
+rsync -a --delete --password-file=/var/rsync_pass_omv /var/yedek/ rsync://msutas@192.168.10.211:873/MARKET-OMV
